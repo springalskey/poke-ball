862e1b980c935e0edd59e872ff1cbc286c3acff6
zhentaoo <1904974327@qq.com>
2016-09-14 17:01:44 +0800

    update kk-views

---
+++ template/directive/index.js
@@ -0,0 +1,17 @@
+/** example **/
+// <h1 v-test="test">nb/list</h1>
+
+import Vue from 'vue';
+
+export default {
+  priority: 110,
+
+  bind: function(el,binding) {
+  },
+
+  update: function() {
+  },
+
+  unbind: function() {
+  }
+};
