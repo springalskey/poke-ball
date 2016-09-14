bccb3effe4fcfa04303dcb8666c22e58fbbc144d
zhentaoo <1904974327@qq.com>
2016-09-14 18:24:16 +0800

    done:create directive

--- tools/path.js
+++ tools/path.js
@@ -8,6 +8,8 @@ exports.SEED_ROOT_PATH = seedRootPath;
 exports.SEED_SRC = path.join(seedRootPath, 'src');
 exports.SEED_SRC_VIEWS = path.join(seedRootPath,'src/views');
 exports.SEED_SRC_VUEX = path.join(seedRootPath,'src/vuex');
+exports.SEED_SRC_DIRECTIVE = path.join(seedRootPath,'src/directive');
+exports.SEED_SRC_COMPONENTS = path.join(seedRootPath,'src/components');
 
 /* cli文件夹路径：*/
 // bin文件执行目录，的上级目录
@@ -18,3 +20,5 @@ exports.CLI_VUE = path.join(cliRootPath, 'vue');
 exports.CLI_TEM = path.join(cliRootPath, 'template');
 exports.CLI_TEM_VIEWS = path.join(cliRootPath, 'template/views');
 exports.CLI_TEM_VUEX = path.join(cliRootPath, 'template/vuex');
+exports.CLI_TEM_DIRECTIVE = path.join(cliRootPath,'template/directive');
+exports.CLI_TEM_COMPONENTS = path.join(cliRootPath,'template/components');
