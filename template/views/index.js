00fab68e1bd174d2b2deade50480956ffb39070c
zhentaoo <1904974327@qq.com>
2016-09-20 11:28:31 +0800

    version 2.0

--- template/views/index.js
+++ template/views/index.js
@@ -1,5 +1,2 @@
-import {upLeafViews} from './{subViews}/{leafViews}';
-
 export default [
-  { path: '/{views}/{subViews}/{leafViews}', component: {upLeafViews}, name: '{views}.{subViews}.{leafViews}' },
 ];
--- template/views/menu.vue
+++ template/views/menu.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
     <el-menu class="second-level-menu" theme="dark" :router="true">
-      <el-submenu index="{leafViews}">
-        <template slot="title"><i class="el-icon-menu"></i>{leafViews}</template>
-        <el-menu-item index="/{views}/{subViews}/{leafViews}" v-kk-permission="'{views}.{subViews}.{leafViews}'">{leafViews}</el-menu-item>
-      </el-submenu>
     </el-menu>
     <router-view class="second-level-view"></router-view>
   </div>
