cbc69b6f39e43d04ba7f4e4e71dc408e622a83d9
yangquan <quan.yang@ele.me>
2016-09-24 17:34:52 +0800

    update template

--- template/views/leafViews/index.vue
+++ template/views/leafViews/index.vue
@@ -8,7 +8,7 @@
     </el-breadcrumb>
   <% } %>
     <el-form
-      class="horizontal-form-item clearfix border-bottom-1"
+      class="horizontal-form-item clearfix"
       @submit.native.prevent="handleSearch(formFilter)"
       :model="formFilter">
   <% config.list.forEach(function (item) { %>
@@ -18,25 +18,27 @@
         <el-input
           v-model="formFilter.<%= item.name %>"
           auto-complete="off"
+          :maxlength="30"
           placeholder="请输入轮<%= item.label %>">
         </el-input>
       </el-form-item>
       <% } else if (item.inputType == 'autocomplete') { %>
-      <el-form-item label="<%= item.label %>" label-width="74px">
+      <el-form-item label="<%= item.label %>" label-width="60px">
         <el-autocomplete
           v-model="formFilter.<%= item.name %>Filter"
           :fetch-suggestions="<%= item.name %>Filter"
+          :trigger-on-focus="false"
           placeholder="请输入<%= item.label %>">
         </el-autocomplete>
       </el-form-item>
       <% } else if (item.inputType == 'switch') {%>
-      <el-form-item label="<%= item.label %>" label-width="74px">
+      <el-form-item label="<%= item.label %>" label-width="60px">
         <el-switch
           v-model="formFilter.<%= item.name %>">
         </el-switch>
       </el-form-item>
       <% } else if (item.inputType == 'date') { %>
-      <el-form-item label="<%= item.label %>" label-width="74px">
+      <el-form-item label="<%= item.label %>" label-width="60px">
         <el-date-picker
           v-model="formFilter.<%= item.name %>"
           type="date"
@@ -44,7 +46,7 @@
         </el-date-picker>
       </el-form-item>
       <% } else if (item.inputType == 'time') { %>
-      <el-form-item label="<%= item.label %>" label-width="74px">
+      <el-form-item label="<%= item.label %>" label-width="60px">
         <el-time-select
           v-model="formFilter.<%= item.name %>"
           :picker-options="{
@@ -56,7 +58,7 @@
         </el-time-select>
       </el-form-item>
       <% } else if (item.inputType == 'datetime') { %>
-      <el-form-item label="<%= item.label %>" label-width="74px">
+      <el-form-item label="<%= item.label %>" label-width="60px">
         <el-date-picker
           v-model="formFilter.<%= item.name %>"
           type="datetime"
@@ -64,13 +66,9 @@
         </el-date-picker>
       </el-form-item>
       <% } else if (item.inputType == 'select') { %>
-      <el-form-item label="<%= item.label %>" label-width="74px">
+      <el-form-item class="form-item-select" label="<%= item.label %>" label-width="90px">
         <el-select v-model="formFilter.<%= item.name %>">
-          <el-option
-            v-for="item in options"
-            :label="item.label"
-            :value="item.value">
-          </el-option>
+          <el-option v-for="item in formFilter.<%= item.name %>" :label="item.label" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <% } else if (item.inputType == 'checkbox') { %>
@@ -90,20 +88,23 @@
   <% } %>
   <% }) %>
       <el-form-item>
-        <el-button type="primary" icon="search">查询</el-button>
+        <el-button type="primary" @click.native.prevent="handleSearch" icon="search">查询</el-button>
       </el-form-item>
     </el-form>
     <el-table
-      :data="posts">
+      :data="{moduleName}s.list">
   <% config.list.forEach(function (item) { %>
     <% if (!item.hasLink) { %>
       <el-table-column property="<%= item.name %>" label="<%= item.label %>">
       </el-table-column>
     <% } else { %>
-      <el-table-column inline-template label="<%= item.label %>">
-        <router-link :to="{ path: '/{moduleName}/' + row.id }">
-          {{<%= item.name %>}}
-        </router-link>
+      <el-table-column
+        inline-template
+        label="操作">
+        <div>
+          <router-link :to="{ path: '/detail/' + row.id }">详情</router-link>
+          <a href="javascript:void(0)" @click="handleDelete(row)">删除</a>
+        </div>
       </el-table-column>
    <% }}) %>
     </el-table>
--- template/views/leafViews/list.js
+++ template/views/leafViews/list.js
@@ -1,10 +1,18 @@
-import Mock from 'resource/mock';
+import util from 'util';
+import {ModuleName}Resrouce from 'resource/{moduleName}';
 
 export default {
+  name: '{ModuleName}List',
   data () {
     return {
-      {moduleName}: [],
-      formFilter: {},
+      formFilter: {
+    <% config.list.forEach(function (item) { %>
+      <% if (item.isFilter) { %>
+        <%= item.name %>: '',
+      <% } %>
+    <% }) %>
+      },
+      {moduleName}s: {},
       pagination: {
         currentPage: 1,
         total: 0,
@@ -14,35 +22,56 @@ export default {
     };
   },
   created () {
-    this.load();
+    this.queryList();
   },
   methods: {
+
     handleSizeChange (size) {
       this.pagination.pageSize = size;
       this.pagination.currentPage = 1;
-      this.load();
+      this.queryList();
     },
+
     handleCurrentChange (index) {
       this.pagination.currentPage = index;
-      this.load();
+      this.queryList();
     },
-    load () {
-      Mock
-        .postsResource()
+    // 列表查询
+    queryList () {
+      {ModuleName}Resrouce()
         .get({
-          _start: (this.pagination.currentPage - 1) * this.pagination.pageSize,
-          _limit: this.pagination.pageSize
+       <% config.list.forEach(function (item) { %>
+        <% if (item.isFilter) { %>
+          <%= item.name %>: this.formFilter.<%= item.name %>,
+        <% } %>
+       <% }) %>
+          size: this.pagination.pageSize,
+          page: (this.pagination.currentPage - 1) * this.pagination.pageSize
         })
         .then(response => {
-          this.pagination.total = 100;
-          this.{moduleName} = response.json();
+          this.{moduleName}s = util.replaceEmptyWithHyphen(response.json());
+          this.pagination.total = this.{moduleName}s.total;
         })
-        .catch(err => {
-          this.$notify({
-            message: '获取列表失败',
-            type: 'error'
-          });
+        .catch(error => { this.$notify({ message: '查询列表数据失败', type: 'error' }); });
+    },
+
+    handleDelete (row) {
+      this.$confirm(`确认删除【${row.name}】吗？`, '提示', { type: 'warning' })
+        .then(() => {
+          {ModuleName}Resrouce()
+            .delete({id: row.id})
+            .then(() => {
+              this.$notify({ message: '删除成功', type: 'success' });
+              this.queryList();
+            })
+            .catch(error => this.$notify({ message: '删除失败', type: 'error' }));
         });
-    }
+    },
+
+    handleSearch () {
+      this.pagination.currentPage = 1;
+      this.pagination.total = 0;
+      this.queryList();
+    },
   }
 };
